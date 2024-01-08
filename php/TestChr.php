d04ef410843f0e1c75c1055777b294fbfa7e6998
mae <mae@chai.finance>
2024-01-08 18:55:32 +0900

    Test php functions
    - chr
    - explode

---
+++ php/TestChr.php
@@ -0,0 +1,10 @@
+<?php
+
+$str = "hello world!";
+$str .= chr(125);
+echo "str : " . $str . "\n";
+
+$bar = chr(240) . chr(122) . chr(10) . chr(45);
+//echo "bar : " . $bar . "\n";
+
+echo chr(333) . "\n";
